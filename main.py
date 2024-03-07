e1ee230ea0aae953edc8207a6e89305e8a65150c
ManexXd <74118310+ManexXd@users.noreply.github.com>
2024-03-07 18:27:54 +0100

    Update main.py

--- main.py
+++ main.py
@@ -22,6 +22,7 @@ time: float = 0.0             # s
 kokePlateEffekt: float = 1.5  # kW
 # Muligheter for virkingsgrader for kokeplaten og kasserollen.
 # Muligheter også for dynamisk varmetap: kJ varmetap som funksjon av temperaturen til kasserollen.
+# Total varmetap i kJ blir: 4.1813 * 1.5 * (siste simulerte vanntemperaturen med ingen tap av varmeenergi) - 4.1813 * 1.5 * (siste vanntemperaturen fra datafil.txt)
 
 
 # Main simulation loop
