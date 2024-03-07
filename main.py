034bb0a1ebc75e05dfdeb4828028bfaaf256e775
ManexXd <74118310+ManexXd@users.noreply.github.com>
2024-03-07 13:39:01 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-# massen til Vann        = 1.50kg
-# massen til Kasserollen = 0.964kg (aluminium)
-# effekten               = 1500W
-# tid                    = 240s
+# Massen til Vann        = 1.50kg
+# Massen til Kasserollen = 0.964kg (aluminium)
+# Effekten               = 1500W
+# Tid                    = 240s
